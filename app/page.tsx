c69c32120556900f3d29e289186190022f0fa3d0
Nuriel Kaunaini Muhammad <nurielkmuhammad1903@gmail.com>
2023-07-25 01:44:39 +0700

    feat: about me and skills added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection"
 import AboutSection from "@/components/AboutSection"
+import ProjectSection from "@/components/ProjectSection"
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
 
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
     </main>
   )
 }
--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,16 +1,35 @@
-"use client"
-import React from 'react'
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+    { skill: "HTML" },
+    { skill: "CSS" },
+    { skill: "JavaScript" },
+    { skill: "Node.js" },
+    { skill: "Express.js" },
+    { skill: "PosgreSQL" },
+    { skill: "MongoDB" },
+    { skill: "Vue.js" },
+    { skill: "React.js" },
+    { skill: "Next.js" },
+    { skill: "Apollo GraphQL" },
+    { skill: "Tailwind CSS" },
+    { skill: "Git" },
+    { skill: "GitHub" },
+]
 
 const AboutSection = () => {
     return (
-        <section id='about'>
-            <div>
-                <h1 className='text-center font-bold text-4xl'>
+        <section id="about">
+            <div className="my-12 pb-12 md:pt-16 md:pb-48">
+                <h1 className="text-center font-bold text-4xl">
                     About Me
+                    <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
                 </h1>
-                <div className='flex flex-col space-y-10 items-stretch justify-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10  '>
-                    <div>
-                        <h1>
+
+                <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+                    <div className="md:w-1/2 ">
+                        <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
                             Let's get acquainted!
                         </h1>
                         <p>
@@ -25,13 +44,20 @@ const AboutSection = () => {
                             After completing my degree, I enrolled in the Fullstack Javascript bootcamp at Hacktiv8 to enhance my programming skills. In just four months, i am now able to construct a client-server based application without a proper background in Computer Engineering.
                         </p>
                     </div>
-                    <div>
-                        <h1>
-                            My skills
-                        </h1>
-                        <p>
-
-                        </p>
+                    <div className="text-center md:w-1/2 md:text-left">
+                        <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+                        <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+                            {skills.map((item, idx) => {
+                                return (
+                                    <p
+                                        key={idx}
+                                        className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                                    >
+                                        {item.skill}
+                                    </p>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
